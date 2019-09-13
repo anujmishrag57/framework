91d9df5d824bbfbce69ad823abae4c9952df63ec
HP <HP@MISHRAG57>
2019-09-13 22:20:28 +0530

    comment removed

--- com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/switchClass.java
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/switchClass.java
@@ -10,30 +10,26 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class switchClass {
-	
-		
+
 	public static void main(String[] args) {
-		
-		//entry set in hash Map
-		
+
 		HashMap<String, Integer> hmap = new HashMap<String, Integer>();
 		hmap.put("A", 1);
 		hmap.put("B", 10);
 		hmap.put("E", 1);
 		hmap.put("C", 100);
 		hmap.put("D", 10000);
-		
+
 		System.out.println(hmap);
 		System.out.println(hmap.size());
-		
-		Set key= hmap.keySet();
-		System.out.println(key);
-		Collection col= hmap.values();
-		System.out.println(col);
-		
-		Set sp= hmap.entrySet();
-		System.out.println(sp);
-		
-}
-}
 
+		Set key = hmap.keySet();
+		System.out.println(key);
+		Collection col = hmap.values();
+		System.out.println(col);
+
+		Set sp = hmap.entrySet();
+		System.out.println(sp);
+
+	}
+}
